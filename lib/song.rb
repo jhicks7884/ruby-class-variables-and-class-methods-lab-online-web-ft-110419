682a129c0416205d2140dec2098e0d37f7c973cf
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-12 13:59:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,15 @@
-class Artist
-end
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genre = []
+  @@artist = []
+
+  def initialize(song_name, artist, genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genre << genre
+    @@artist << artist
+  end
